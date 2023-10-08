5754b654283156e6e70a15b5f2ef8f62ac772404
tofu-tigre <jaeden.quintana@gmail.com>
2023-10-08 12:33:05 -0700

    Refactored code.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::error::Error;
-use std::io::ErrorKind;
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::RwLock;
-use std::thread;
 
 
 use std::net::{
@@ -19,12 +17,7 @@ use tokio::{
 
 #[tokio::main]
 async fn main() {
-    let task_type = "upload";
-    match task_type {
-        "download" => { todo!(); }
-        "upload" => { let _ = run_server().await; }
-        &_ => { todo!(); }
-    };
+    let _ = run_server().await.unwrap();
 }
 
 /// Spawns a new thread that deals with user uploads.
@@ -32,39 +25,20 @@ async fn main() {
 async fn run_server() -> Result<(), Box<dyn Error>> {
     let files: Arc<RwLock<HashMap<String, Vec<u8>>>> =
         Arc::new(RwLock::new(HashMap::new()));
-    
-    spawn_upload_task(files.clone()).await?;
-    spawn_client_listener_tasks(String::from("localhost::8080"), files.clone()).await?;
-    unreachable!()
-}
 
-async fn spawn_client_listener_tasks(
-    addr: String, 
-    files_handle: Arc<RwLock<HashMap<String, Vec<u8>>>>) ->
-    Result<(), Box<dyn Error>> {
-    
-    let listener = TcpListener::bind(addr.as_str()).await?;
     loop {
-        let fh = files_handle.clone();
-        let (socket, addr) = listener.accept().await?;
-        println!("Client {addr} connected to server");
-        tokio::spawn(async move {
-            let _ = echo(socket, addr, fh).await;
-        });
+        // TODO: Catch dead tasks here!
+        let listener = TcpListener::bind("localhost:8080").await?;
+        spawn_client_listener_task(&listener, files.clone()).await?;
     }
 }
 
-
-async fn spawn_upload_task(files_handle: Arc<RwLock<HashMap<String, Vec<u8>>>>) ->
+async fn spawn_client_listener_task(listener: &TcpListener, files_handle: Arc<RwLock<HashMap<String, Vec<u8>>>>) ->
     Result<(), Box<dyn Error>> {
+    let (socket, addr) = listener.accept().await?;
+    println!("Client {addr} connected to server");
     tokio::spawn(async move {
-        loop {
-            let line = String::new();
-            if line.as_str() == "exit" {
-                let mut rw_guard = files_handle.write().await;
-                rw_guard.insert(String::from("Foo"), b"foo".to_vec());
-            }
-        }
+        let _ = echo(socket, addr, files_handle).await;
     });
     Ok(())
 }
